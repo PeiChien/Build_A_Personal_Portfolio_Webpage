36a7c1bd022e81ffd6f3885a24726f573d30e2d3
PeiChien <925846753@qq.com>
2019-03-17 16:49:50 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const MiniCssExtractPlugin=require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports={
+    entry:'./src/main.js',
+    output:{
+        filename:'bundle.js',
+        path:path.resolve(__dirname,'./dist')
+    },
+    mode:"development",
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[MiniCssExtractPlugin.loader,'css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+            minify: {
+                removeAttributeQuotes: false, //删除属性双引号
+                collapseWhitespace: false //变成一行
+            },
+            hash: false
+        }),
+        new MiniCssExtractPlugin({
+            filename:'main.css'
+        })
+    ]
+};
