0443d9290bc2e79dcf7d28b6f343a85e84d343fd
PeiChien <925846753@qq.com>
2019-03-19 12:05:48 +0800

    Create 难度选择.js

---
+++ src/js/贪吃蛇/难度选择.js
@@ -0,0 +1,32 @@
+document.getElementById("easy").onclick = function () {
+    document.getElementById("easy").disabled = true;
+    document.getElementById("common").disabled = true;
+    document.getElementById("hard").disabled = true;
+    //初始化游戏对象
+    var gm = new Game(document.getElementById("Contact"), 150);
+
+    //开始游戏
+    gm.init();
+};
+
+document.getElementById("common").onclick = function () {
+    document.getElementById("easy").disabled = true;
+    document.getElementById("common").disabled = true;
+    document.getElementById("hard").disabled = true;
+    //初始化游戏对象
+    var gm = new Game(document.getElementById("Contact"), 100);
+
+    //开始游戏
+    gm.init();
+};
+
+document.getElementById("hard").onclick = function () {
+    document.getElementById("easy").disabled = true;
+    document.getElementById("common").disabled = true;
+    document.getElementById("hard").disabled = true;
+    //初始化游戏对象
+    var gm = new Game(document.getElementById("Contact"), 15);
+
+    //开始游戏
+    gm.init();
+};
