d945f75545c2c8211781ed5baf18c8e9af713107
PeiChien <925846753@qq.com>
2019-03-25 19:42:35 +0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
-require('../css/main.css');
-require('./页面跳转/点击跳转.js');
-require('./贪吃蛇/食物.js');
-require('./贪吃蛇/小蛇.js');
-require('./贪吃蛇/游戏对象.js');
-require('./贪吃蛇/难度选择.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//导入第三方样式表
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'font-awesome/css/font-awesome.min.css';
+
+//导入各部分模块
+import NavBar from '@/components/NavBar';
+import MainParts from '@/components/MainParts';
+import MyInfo from '@/components/MyInfo';
+import Footer from '@/components/Footer';
+
+//导入js动画文件
+import '@/页面跳转/点击跳转.js';
+import '@/贪吃蛇/难度选择';
+
+ReactDOM.render(
+<div>
+<NavBar></NavBar>
+<MainParts></MainParts>
+<MyInfo></MyInfo>
+<Footer></Footer>
+</div>,document.getElementById('app'))
+
+
